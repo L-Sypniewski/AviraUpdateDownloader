8af16c7448cfef60e3c9e0a2f2dd49e91f5427cc
Łukasz Sypniewski <l.sypniewski@gmail.com>
2017-09-18 21:03:54 +0200

    Add files via upload

---
+++ AviraUpdater/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace AviraUpdater
+{
+    partial class MainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainFrm));
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(339, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Pobierz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(339, 38);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // MainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(339, 61);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainFrm";
+            this.Text = "Avira Aktualizator";
+            this.Activated += new System.EventHandler(this.MainFrm_Activated);
+            this.Load += new System.EventHandler(this.MainFrm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ AviraUpdater/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using Microsoft.WindowsAPICodePack.Taskbar;
+using Microsoft.WindowsAPICodePack.Shell;
+using System.IO;
+namespace AviraUpdater
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            InitializeComponent();
+        }
+        private TaskbarManager windowsTaskbar = TaskbarManager.Instance;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string systemFolder = Environment.GetFolderPath(Environment.SpecialFolder.System);
+
+            IJumpListTask notepadTask = new JumpListLink(System.IO.Path.Combine(systemFolder, "notepad.exe"), "Open Notepad")
+            {
+                IconReference = new IconReference(Path.Combine(systemFolder, "notepad.exe"), 0)
+            };
+            IJumpListTask calcTask = new JumpListLink(Path.Combine(systemFolder, "calc.exe"), "Open Calculator")
+            {
+                IconReference = new IconReference(Path.Combine(systemFolder, "calc.exe"), 0)
+            };
+            IJumpListTask paintTask = new JumpListLink(Path.Combine(systemFolder, "mspaint.exe"), "Open Paint")
+            {
+                IconReference = new IconReference(Path.Combine(systemFolder, "mspaint.exe"), 0)
+            };
+
+            // JumpList.AddUserTasks(notepadTask, calcTask, new JumpListSeparator(), paintTask);
+            // JumpList.Refresh();
+
+
+
+            if (button1.Text != "Zakończ")
+            {
+                button1.Enabled = false;
+
+                Icon Ikona = Icon.ExtractAssociatedIcon(Application.StartupPath + "\\avira_by_faton.ico");
+                TaskbarManager.Instance.SetOverlayIcon(Ikona, "Ikona");
+
+                WebClient webClient = new WebClient();
+                webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+                webClient.DownloadFileAsync(new Uri("http://dl.antivir.de/down/vdf/ivdf_fusebundle_nt_en.zip"), System.Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\ivdf_fusebundle_nt_en.zip");
+            }
+            else Application.Exit();
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            const string AppName = "Avira Aktualizator";
+            progressBar1.Value = e.ProgressPercentage;
+            double KiloBytesReceived, TotalKiloBytesToReceive;
+            KiloBytesReceived = e.BytesReceived / 1024;
+            TotalKiloBytesToReceive = e.TotalBytesToReceive / 1024;
+            this.Text = KiloBytesReceived.ToString() + "Kb" + " / " + TotalKiloBytesToReceive + "Kb" + " - " + AppName;
+            windowsTaskbar.SetProgressValue(e.ProgressPercentage, 100);
+            if (e.ProgressPercentage != 100)
+            {
+                button1.Enabled = false;
+            }
+            else
+            {
+                button1.Enabled = true;
+            }
+
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            this.Activate();
+            button1.Text = "Zakończ";
+
+        }
+
+        private void MainFrm_Activated(object sender, EventArgs e)
+        {
+
+            if (TaskbarManager.IsPlatformSupported == false)
+            {
+                MessageBox.Show("System nie obsługuje Paska Zadań z Windows 7 lub nowszych!", "Pełna fuckjonalność nie jest możliwa!", MessageBoxButtons.OK, MessageBoxIcon.Error); Application.Exit();
+            }
+        }
+
+        private void MainFrm_Load(object sender, EventArgs e)
+        {
+            JumpList jump = JumpList.CreateJumpList();
+            jump.AddUserTasks(new JumpListLink("C:\\Program Files (x86)\\CCleaner\\CCleaner.exe", "Uruchom CCleanera")
+            {
+                IconReference = new IconReference("C:\\Program Files (x86)\\CCleaner\\CCleaner.exe", 0)
+                
+            });
+            jump.Refresh();
+
+        }
+    }
+}
+
